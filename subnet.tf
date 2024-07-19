dd63479f36cdfb16df034a8ed540581765bd49cb
omera <omera@netapp.com>
2024-07-19 19:36:20 +0300

    [src] more

--- subnet.tf
+++ subnet.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.main_vpn.id
   cidr_block              = "10.0.0.16/28" #16 IPs
   map_public_ip_on_launch = false         # private subnet
-  availability_zone       = "us-east-1b"
+  availability_zone       = "us-east-1a"
     tags = {
         Name = "omer-private-subnet"
     }
